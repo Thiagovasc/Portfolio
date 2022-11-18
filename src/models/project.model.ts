cbc96e94eba3145caa609239c4bb78d13967904d
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-11-18 19:00:56 -0300

    initial components commit

---
+++ src/models/project.model.ts
@@ -0,0 +1,11 @@
+export class Project {
+    constructor(
+        public title: string,
+        public description: string,
+        public stack: object[],
+        public link: string
+
+    ){
+
+    }
+}
